4a29b00369eaf23ab13348168d099d2a8949a9df
arp226 <arp226@njit.edu>
2018-02-14 01:38:37 -0500

    updated Driver and added Reducer

--- src/MissingCardsDriver.java
+++ src/MissingCardsDriver.java
@@ -1,27 +1,41 @@
-import org.apache.hadoop.conf.Configuration;
+
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
 
-public class MissingCardsDriver{
+public class MissingCardsDriver {
 
 	public static void main(String[] args) throws Exception {
+		JobConf conf = new JobConf(MissingCardsDriver.class);
+		conf.setJobName("Missing Cards Job");
+		conf.setOutputKeyClass(Text.class);
+		conf.setOutputValueClass(IntWritable.class);
+		conf.setMapperClass(MissingCardsMapper.class);
+		conf.setReducerClass(MissingCardsReducer.class);
+		conf.setInputFormat(TextInputFormat.class);
+		conf.setOutputFormat(TextOutputFormat.class);
+		FileInputFormat.setInputPaths(conf, new Path(args[0]));
+		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+		JobClient.runJob(conf);
 
-		Job job = Job.getInstance(new Configuration(), "Missing Cards Job");
-		job.setJarByClass(MissingCardsDriver.class);
-		TextInputFormat.addInputPath(job, new Path(args[0]));
-		job.setInputFormatClass(TextInputFormat.class);
-		job.setMapperClass(MissingCardsMapper.class);
-		job.setReducerClass(MissingCardsReducer.class);
-		TextOutputFormat.setOutputPath(job, new Path(args[1]));
-		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(IntWritable.class);
-		job.setOutputFormatClass(TextOutputFormat.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
-		System.exit(job.waitForCompletion(true) ? 0 : -1);
+		// Job job = Job.getInstance(new Configuration(), "Missing Cards Job");
+		// job.setJarByClass(MissingCardsDriver.class);
+		// TextInputFormat.addInputPath(job, new Path(args[0]));
+		// job.setInputFormatClass(TextInputFormat.class);
+		// job.setMapperClass(MissingCardsMapper.class);
+		// job.setReducerClass(MissingCardsReducer.class);
+		// TextOutputFormat.setOutputPath(job, new Path(args[1]));
+		// job.setMapOutputKeyClass(Text.class);
+		// job.setMapOutputValueClass(IntWritable.class);
+		// job.setOutputFormatClass(TextOutputFormat.class);
+		// job.setOutputKeyClass(Text.class);
+		// job.setOutputValueClass(Text.class);
+		// System.exit(job.waitForCompletion(true) ? 0 : -1);
 	}
 }
--- src/MissingCardsReducer.java
+++ src/MissingCardsReducer.java
@@ -1,4 +1,26 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
-public class MissingCardsReducer {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+public class MissingCardsReducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, Text> {
+
+	@Override
+	public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, Text> output,
+			Reporter reporter) throws IOException {
+		List<Integer> valueList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+		while (values.hasNext()) {
+			valueList.remove(values.next());
+
+		}
+		 output.collect(key, new Text(valueList.toString()));
+	}
 
 }
