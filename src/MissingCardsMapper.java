91388f29e400cb69660648aa338cd73797d64db0
arp226 <arp226@njit.edu>
2018-02-14 01:04:56 -0500

    Added Missign cards Mapper .map functionality

--- src/MissingCardsMapper.java
+++ src/MissingCardsMapper.java
@@ -1,4 +1,27 @@
 
-public class MissingCardsMapper {
+import java.io.IOException;
+import java.util.StringTokenizer;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+//Mapper class 
+public class MissingCardsMapper extends MapReduceBase implements
+		Mapper<Text, /* Input key Type */
+				Text, /* Input value Type */
+				Text, /* Output key Type */
+				IntWritable> /* Output value Type */
+{
+
+	// Map function
+	public void map(Text key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter)
+			throws IOException {
+		String textLine = value.toString();
+		String arr[] = textLine.split(",");
+		output.collect(new Text(arr[0]), new IntWritable(Integer.parseInt(arr[1])));
+	}
 }
