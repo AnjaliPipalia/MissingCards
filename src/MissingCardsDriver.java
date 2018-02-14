fbe876c2d21dc1d834cc0edd81bff450139336ff
arp226 <arp226@njit.edu>
2018-02-14 00:24:58 -0500

    created job instances,adding input path,set path for Mapper and Reducer class, set input&output Key/Value class

--- src/MissingCardsDriver.java
+++ src/MissingCardsDriver.java
@@ -1,9 +1,27 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
-public class MissingCardsDriver {
+public class MissingCardsDriver{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
 
+		Job job = Job.getInstance(new Configuration(), "Missing Cards Job");
+		job.setJarByClass(MissingCardsDriver.class);
+		TextInputFormat.addInputPath(job, new Path(args[0]));
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setMapperClass(MissingCardsMapper.class);
+		job.setReducerClass(MissingCardsReducer.class);
+		TextOutputFormat.setOutputPath(job, new Path(args[1]));
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		System.exit(job.waitForCompletion(true) ? 0 : -1);
 	}
-
 }
